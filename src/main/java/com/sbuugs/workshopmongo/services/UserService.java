127c6d24cf940cd2d0079cfc66513b9920a7a441
sbuugs <ggubs0816@gmail.com>
2022-02-11 17:51:49 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/sbuugs/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.sbuugs.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sbuugs.workshopmongo.domain.User;
+import com.sbuugs.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+
+}
