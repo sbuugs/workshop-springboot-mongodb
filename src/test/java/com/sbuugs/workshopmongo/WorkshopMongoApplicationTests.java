03dc08275d2331c5940d4ee568432db95d0648a8
sbuugs <ggubs0816@gmail.com>
2022-02-11 17:11:43 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/sbuugs/workshopmongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbuugs.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
