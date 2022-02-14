c8afa1db031638a47ed5bab18dfbc4a7ef2231b6
sbuugs <ggubs0816@gmail.com>
2022-02-14 16:12:35 -0300

    Inserção de usuário com POST

--- src/main/java/com/sbuugs/workshopmongo/services/UserService.java
+++ src/main/java/com/sbuugs/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sbuugs.workshopmongo.domain.User;
+import com.sbuugs.workshopmongo.dto.UserDTO;
 import com.sbuugs.workshopmongo.repository.UserRepository;
 import com.sbuugs.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDto(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
