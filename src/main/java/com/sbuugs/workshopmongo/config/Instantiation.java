023a7d87f2aa6ed8f0eefd52ebb26c2ba7250fef
sbuugs <ggubs0816@gmail.com>
2022-02-14 17:31:05 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/sbuugs/workshopmongo/config/Instantiation.java
+++ src/main/java/com/sbuugs/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.sbuugs.workshopmongo.domain.Post;
 import com.sbuugs.workshopmongo.domain.User;
+import com.sbuugs.workshopmongo.dto.AuthorDTO;
 import com.sbuugs.workshopmongo.repository.PostRepository;
 import com.sbuugs.workshopmongo.repository.UserRepository;
 
@@ -35,12 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("11/02/2022"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("13/02/2022"), "Bom dia!", "Acordei feliz hoje.", maria);
-		
-		postRepository.saveAll(Arrays.asList(post1, post2));
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
+		Post post1 = new Post(null, sdf.parse("11/02/2022"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("13/02/2022"), "Bom dia!", "Acordei feliz hoje.", new AuthorDTO(maria));
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
