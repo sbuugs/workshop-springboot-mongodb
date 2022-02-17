604af1c4d01ac322f2d2b34cecb1ae14d0210680
sbuugs <ggubs0816@gmail.com>
2022-02-17 14:50:16 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/sbuugs/workshopmongo/config/Instantiation.java
+++ src/main/java/com/sbuugs/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.sbuugs.workshopmongo.domain.Post;
 import com.sbuugs.workshopmongo.domain.User;
 import com.sbuugs.workshopmongo.dto.AuthorDTO;
+import com.sbuugs.workshopmongo.dto.CommentDTO;
 import com.sbuugs.workshopmongo.repository.PostRepository;
 import com.sbuugs.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,15 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("11/02/2022"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("13/02/2022"), "Bom dia!", "Acordei feliz hoje.", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem!", sdf.parse("21/01/2022"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/01/2022"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/01/2022"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
